069ff67cf869731af0d57991f1c1db2f1922a36e
Marek Koza <qyx@krtko.org>
2014-11-19 00:48:40 +0100

    Initial import. Add line editor basic functionality (without history recall).

---
+++ lineedit.c
@@ -0,0 +1,475 @@
+/**
+ * Copyright (c) 2014, Marek Koza (qyx@krtko.org)
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+#include <malloc.h>
+#include <inttypes.h>
+
+#include "lineedit.h"
+
+
+int32_t lineedit_escape_print(struct lineedit *le, enum lineedit_escape_seq esc, int param) {
+	if (u_assert(le != NULL) ||
+	    u_assert(le->print_handler != NULL)) {
+		return LINEEDIT_ESCAPE_PRINT_FAILED;
+	}
+
+	char s[20];
+	switch (esc) {
+		case ESC_CURSOR_LEFT:
+			le->print_handler("\x1b[D", le->print_handler_ctx);
+			break;
+		case ESC_CURSOR_RIGHT:
+			le->print_handler("\x1b[C", le->print_handler_ctx);
+			break;
+		case ESC_COLOR:
+			snprintf(s, sizeof(s), "\x1b[%dm", param);
+			le->print_handler(s, le->print_handler_ctx);
+			break;
+		case ESC_DEFAULT:
+			le->print_handler("\x1b[0m", le->print_handler_ctx);
+			break;
+		case ESC_BOLD:
+			le->print_handler("\x1b[1m", le->print_handler_ctx);
+			break;
+		case ESC_CURSOR_SAVE:
+			le->print_handler("\x1b[s", le->print_handler_ctx);
+			break;
+		case ESC_CURSOR_RESTORE:
+			le->print_handler("\x1b[u", le->print_handler_ctx);
+			break;
+		case ESC_ERASE_LINE_END:
+			le->print_handler("\x1b[K", le->print_handler_ctx);
+			break;
+		default:
+			return LINEEDIT_ESCAPE_PRINT_FAILED;
+	}
+
+	return LINEEDIT_ESCAPE_PRINT_OK;
+}
+
+
+int32_t lineedit_init(struct lineedit *le, uint32_t line_len) {
+	if (u_assert(le != NULL) ||
+	    u_assert(line_len > 0)) {
+		return LINEEDIT_INIT_FAILED;
+	}
+
+	le->text = malloc(line_len);
+	if (le->text == NULL) {
+		return LINEEDIT_INIT_FAILED;
+	}
+
+	le->len = line_len;
+	le->cursor = 0;
+	le->text[0] = 0;
+	le->pwchar = '\0';
+	le->prompt_len = 0;
+	le->csi_escape_mod = 0;
+	le->print_handler = NULL;
+	le->print_handler_ctx = NULL;
+	le->prompt_callback = NULL;
+
+	return LINEEDIT_INIT_OK;
+}
+
+
+int32_t lineedit_free(struct lineedit *le) {
+	if (u_assert(le != NULL) ||
+	    u_assert(le->text != NULL)) {
+		return LINEEDIT_FREE_FAILED;
+	}
+
+	free(le->text);
+
+	return LINEEDIT_FREE_OK;
+}
+
+
+int32_t lineedit_keypress(struct lineedit *le, int c) {
+	if (u_assert(le != NULL) ||
+	    u_assert(le->print_handler != NULL)) {
+		return LINEEDIT_FAILED;
+	}
+
+	/* check for TAB */
+	if (c == 0x09) {
+		return LINEEDIT_TAB;
+	}
+
+	/* check for line feed */
+	if (c == 0x0a || c == 0x0b || c == 0x0c || c == 0x0d) {
+		return LINEEDIT_ENTER;
+	}
+
+	/* interrupt escape sequence */
+	if (c == 0x18 || c == 0x1a) {
+		le->escape = ESC_NONE;
+		return LINEEDIT_OK;
+	}
+
+	/* check for ESC */
+	if (c == 0x1b) {
+		le->escape = ESC_ESC;
+		return LINEEDIT_OK;
+	}
+
+	/* check for DEL (backspace) */
+	if (c == 0x7f) {
+		/* Do not check return value, if we are unable to do backspace,
+		 * we just ignore it. */
+		lineedit_backspace(le);
+		return LINEEDIT_OK;
+	}
+
+	/* check for CSI */
+	if (c == 0x9b) {
+		le->escape = ESC_CSI;
+		le->csi_escape_mod = 0;
+		return LINEEDIT_OK;
+	}
+
+	/* if ESC is set and '[' character was received, start CSI sequence */
+	if (le->escape == ESC_ESC && c == '[') {
+		le->escape = ESC_CSI;
+		le->csi_escape_mod = 0;
+		return LINEEDIT_OK;
+	}
+
+	/* if ESC is set and ']' character was received, start OSC sequence */
+	if (le->escape == ESC_ESC && c == ']') {
+		le->escape = ESC_OSC;
+		return LINEEDIT_OK;
+	}
+
+	/* if CSI is set, try to read first alphanumeric character (parameters are ignored */
+	if (le->escape == ESC_CSI) {
+		/* Escape modifier, we continue with CSI escape flag set. */
+		if (c >= '0' && c <= '9') {
+			le->csi_escape_mod = le->csi_escape_mod * 10 + (c - '0');
+			return LINEEDIT_OK;
+		}
+
+		if (c == 'A') {
+			/* Move cursor up (previous history entry). */
+			/* TODO: */
+		}
+		if (c == 'B') {
+			/* Move cursor down (next history entry). */
+			/* TODO: */
+		}
+		if (c == 'C') {
+			/* move cursor right */
+			if (le->cursor < (strlen(le->text))) {
+				le->cursor++;
+				lineedit_escape_print(le, ESC_CURSOR_RIGHT, 1);
+			}
+		}
+		if (c == 'D') {
+			/* move cursor left */
+			if (le->cursor > 0) {
+				le->cursor--;
+				lineedit_escape_print(le, ESC_CURSOR_LEFT, 1);
+			}
+		}
+		if (c == '~') {
+			/* Delete key. */
+			lineedit_backspace(le);
+		}
+
+		le->escape = ESC_NONE;
+		return LINEEDIT_OK;
+	}
+
+
+	/* other alphanumeric characters */
+	if (c >= 32 && c <= 127) {
+		/* Do not check return value, if we are unable to insert it,
+		 * we just ignore the character. */
+		lineedit_insert_char(le, c);
+		return LINEEDIT_OK;
+	}
+
+	return LINEEDIT_OK;
+}
+
+
+int32_t lineedit_backspace(struct lineedit *le) {
+	if (u_assert(le != NULL)) {
+		return LINEEDIT_BACKSPACE_FAILED;
+	}
+
+	/* we are going to remove 1 character at cursor position,
+	 * check if we have anything to remove */
+	if (strlen(le->text) == 0 || le->cursor == 0) {
+		return LINEEDIT_BACKSPACE_FAILED;
+	}
+
+	/* move cursor left */
+	le->cursor--;
+	lineedit_escape_print(le, ESC_CURSOR_LEFT, 1);
+
+	/* shift line left */
+	int32_t i = le->cursor;
+	while (le->text[i] != 0) {
+		le->text[i] = le->text[i + 1];
+		i++;
+	}
+
+	/* save cursor position */
+	lineedit_escape_print(le, ESC_CURSOR_SAVE, 0);
+
+	/* now we need to refresh rest of the line */
+	i = le->cursor;
+	while (le->text[i]) {
+		char line[2] = {le->text[i], '\0'};
+		le->print_handler(line, le->print_handler_ctx);
+		i++;
+	}
+
+	/* erase everything to the end of current line */
+	lineedit_escape_print(le, ESC_ERASE_LINE_END, 0);
+
+	/* restore cursor position */
+	lineedit_escape_print(le, ESC_CURSOR_RESTORE, 0);
+
+	return LINEEDIT_BACKSPACE_OK;
+}
+
+
+int32_t lineedit_insert_char(struct lineedit *le, int c) {
+	if (u_assert(le != NULL)) {
+		return LINEEDIT_INSERT_CHAR_FAILED;
+	}
+
+	/* Only printable characters can be inserted. */
+	if (c < 32 || c > 127) {
+		return LINEEDIT_INSERT_CHAR_FAILED;
+	}
+
+	/* we are going to insert 1 character, check if we have enough space */
+	if ((le->len - strlen(le->text) - 1) <= 0) {
+		return LINEEDIT_INSERT_CHAR_FAILED;;
+	}
+
+	int32_t i = strlen(le->text);
+	while (i >= (int32_t)le->cursor) {
+		le->text[i + 1] = le->text[i];
+		i--;
+	}
+
+	/* set character at cursor */
+	le->text[le->cursor] = c;
+
+	/* and increment cursor */
+	le->cursor++;
+
+	/* print character at cursor position */
+	char line[2] = {(le->pwchar != 0) ? le->pwchar : c, '\0'};
+	le->print_handler(line, le->print_handler_ctx);
+
+	/* save cursor position */
+	lineedit_escape_print(le, ESC_CURSOR_SAVE, 0);
+
+	/* now we need to refresh rest of the line */
+	i = le->cursor;
+	while (le->text[i]) {
+		char line[2] = {(le->pwchar != 0) ? le->pwchar : le->text[i], '\0'};
+		le->print_handler(line, le->print_handler_ctx);
+		i++;
+	}
+
+	/* restore cursor position */
+	lineedit_escape_print(le, ESC_CURSOR_RESTORE, 0);
+
+	return LINEEDIT_INSERT_CHAR_OK;
+}
+
+
+int32_t lineedit_set_print_handler(struct lineedit *le, int32_t (*print_handler)(const char *line, void *ctx), void *ctx) {
+	if (u_assert(le != NULL) ||
+	    u_assert(print_handler != NULL)) {
+		return LINEEDIT_SET_PRINT_HANDLER_FAILED;
+	}
+
+	le->print_handler = print_handler;
+	le->print_handler_ctx = ctx;
+
+	return LINEEDIT_SET_PRINT_HANDLER_OK;
+}
+
+
+int32_t lineedit_set_prompt_callback(struct lineedit *le, int32_t (*prompt_callback)(struct lineedit *le, void *ctx), void *ctx) {
+	if (u_assert(le != NULL) ||
+	    u_assert(prompt_callback != NULL)) {
+		return LINEEDIT_SET_PROMPT_CALLBACK_FAILED;
+	}
+
+	le->prompt_callback = prompt_callback;
+	le->prompt_callback_ctx = ctx;
+
+	return LINEEDIT_SET_PROMPT_CALLBACK_OK;
+}
+
+
+int32_t lineedit_refresh(struct lineedit *le) {
+	if (u_assert(le != NULL) ||
+	    u_assert(le->print_handler != NULL)) {
+		return LINEEDIT_REFRESH_FAILED;
+	}
+
+	uint32_t saved = 0;
+
+	/* move cursor to start */
+	le->print_handler("\r", le->print_handler_ctx);
+
+	/* erase whole line */
+	lineedit_escape_print(le, ESC_ERASE_LINE_END, 0);
+
+	if (le->prompt_callback != NULL) {
+		le->prompt_len = le->prompt_callback(le, le->prompt_callback_ctx);
+		/* negative number returned, error occured */
+		if (le->prompt_len < 0) {
+			le->prompt_len = 0;
+		}
+	}
+
+	uint32_t i = 0;
+	while (le->text[i] != '\0') {
+		if (le->cursor == i) {
+			/* save cursor position */
+			lineedit_escape_print(le, ESC_CURSOR_SAVE, 0);
+			saved = 1;
+		}
+
+		char line[2] = {le->text[i], '\0'};
+		le->print_handler(line, le->print_handler_ctx);
+
+		i++;
+	}
+
+	/* restore cursor position if needed */
+	if (saved) {
+		lineedit_escape_print(le, ESC_CURSOR_RESTORE, 0);
+	}
+
+	return LINEEDIT_REFRESH_OK;
+}
+
+
+int32_t lineedit_get_cursor(struct lineedit *le, uint32_t *cursor) {
+	if (u_assert(le != NULL) ||
+	    u_assert(cursor != NULL)) {
+		return LINEEDIT_GET_CURSOR_FAILED;
+	}
+
+	*cursor = le->cursor;
+
+	return LINEEDIT_GET_CURSOR_OK;
+}
+
+
+/* TODO: can be implemented more effectively. */
+int32_t lineedit_set_cursor(struct lineedit *le, uint32_t cursor) {
+	if (u_assert(le != NULL)) {
+		return LINEEDIT_SET_CURSOR_FAILED;
+	}
+
+	if (cursor > strlen(le->text)) {
+		return LINEEDIT_SET_CURSOR_FAILED;
+	}
+
+	le->cursor = cursor;
+
+	/* move cursor to start */
+	le->print_handler("\r", le->print_handler_ctx);
+
+	/* Move cursor to the right up to requested cursor position. */
+	for (uint32_t i = 0; i < le->cursor; i++) {
+		lineedit_escape_print(le, ESC_CURSOR_RIGHT, 0);
+	}
+
+	return LINEEDIT_SET_CURSOR_OK;
+
+}
+
+
+int32_t lineedit_get_line(struct lineedit *le, char **text) {
+	if (u_assert(le != NULL) ||
+	    u_assert(text != NULL)) {
+		return LINEEDIT_GET_LINE_FAILED;
+	}
+
+	*text = le->text;
+
+	return LINEEDIT_GET_LINE_OK;
+}
+
+
+int32_t lineedit_set_line(struct lineedit *le, const char *text) {
+	if (u_assert(le != NULL) ||
+	    u_assert(text != NULL)) {
+		return LINEEDIT_SET_LINE_FAILED;
+	}
+
+	strncpy(le->text, text, le->len);
+	le->text[le->len - 1] = 0;
+
+	return LINEEDIT_SET_LINE_OK;
+}
+
+
+int32_t lineedit_clear(struct lineedit *le) {
+	if (u_assert(le != NULL)) {
+		return LINEEDIT_CLEAR_FAILED;
+	}
+
+	le->text[0] = '\0';
+	le->cursor = 0;
+
+	return LINEEDIT_CLEAR_OK;
+}
+
+
+int32_t lineedit_insert(struct lineedit *le, const char *text) {
+	if (u_assert(le != NULL) ||
+	    u_assert(text != NULL)) {
+		return LINEEDIT_INSERT_FAILED;
+	}
+
+	while (*text) {
+		lineedit_insert_char(le, *text);
+		text++;
+	}
+
+	return LINEEDIT_INSERT_OK;
+}
+
+
+
+
---
+++ lineedit.h
@@ -0,0 +1,189 @@
+/**
+ * Copyright (c) 2014, Marek Koza (qyx@krtko.org)
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef _LINEEDIT_H_
+#define _LINEEDIT_H_
+
+
+/**
+ * Custom assert definition. In an embedded environment, it can be made void or
+ * modified according to custom needs.
+ */
+#ifndef u_assert
+#define u_assert(e) ((e) ? (0) : (printf("Assertion '%s' failed in %s, line %d\n", #e, __FILE__, __LINE__), abort(), 1))
+#endif
+
+
+/**
+ * Foreground color parameter definitions used as arguments to
+ * @a lineedit_escape_print function.
+ */
+#define LINEEDIT_FG_COLOR_BLACK 30
+#define LINEEDIT_FG_COLOR_RED 31
+#define LINEEDIT_FG_COLOR_GREEN 32
+#define LINEEDIT_FG_COLOR_YELLOW 33
+#define LINEEDIT_FG_COLOR_BLUE 34
+#define LINEEDIT_FG_COLOR_MAGENTA 35
+#define LINEEDIT_FG_COLOR_CYAN 36
+#define LINEEDIT_FG_COLOR_WHITE 37
+
+
+/**
+ * Output escape sequence passed as an argument to @a lineedit_escape_print
+ * function.
+ */
+enum lineedit_escape_seq {
+	ESC_CURSOR_LEFT,
+	ESC_CURSOR_RIGHT,
+	ESC_COLOR,
+	ESC_DEFAULT,
+	ESC_BOLD,
+	ESC_CURSOR_SAVE,
+	ESC_CURSOR_RESTORE,
+	ESC_ERASE_LINE_END
+};
+
+
+enum lineedit_escape {
+	ESC_NONE, ESC_ESC, ESC_CSI, ESC_OSC
+};
+
+
+/**
+ * Line editor context structure. All lineedit operations need this struct as
+ * their first argument.
+ */
+struct lineedit {
+	/**
+	 * Actual cursor position. Valid during line ediding.
+	 */
+	uint32_t cursor;
+
+	/**
+	 * Pointer to line buffer of @a len length. It is used to store last
+	 * (actually edited) line.
+	 */
+	char *text;
+	uint32_t len;
+
+	/**
+	 * Input terminal/console escape sequence state. @a csi_escape_mod is
+	 * valid only if @a escape equals ESC_CSI.
+	 */
+	enum lineedit_escape escape;
+	uint32_t csi_escape_mod;
+
+	/**
+	 * Optional charater to be substituted for all printed characters.
+	 * Set to non-zero value if a password-like editor is desired.
+	 */
+	char pwchar;
+
+	/**
+	 * Function called whenever there's a need to print anything to editor
+	 * output terminal/console. Needs to be set after context initialization
+	 * and before using keypress function. @a ctx is passed as an argument
+	 * to @a print_handler function.
+	 */
+	int32_t (*print_handler)(const char *line, void *ctx);
+	void *print_handler_ctx;
+
+	/**
+	 * Function called when a line command prompt (a beginning of edited line)
+	 * should be printed. @a ctx is passed as an argument to @a prompt_callback.
+	 */
+	int32_t (*prompt_callback)(struct lineedit *le, void *ctx);
+	void *prompt_callback_ctx;
+	uint32_t prompt_len;
+};
+
+
+int32_t lineedit_escape_print(struct lineedit *le, enum lineedit_escape_seq esc, int param);
+#define LINEEDIT_ESCAPE_PRINT_OK 0
+#define LINEEDIT_ESCAPE_PRINT_FAILED -1
+
+int32_t lineedit_init(struct lineedit *le, uint32_t line_len);
+#define LINEEDIT_INIT_OK 0
+#define LINEEDIT_INIT_FAILED -1
+
+int32_t lineedit_free(struct lineedit *le);
+#define LINEEDIT_FREE_OK 0
+#define LINEEDIT_FREE_FAILED -1
+
+int32_t lineedit_keypress(struct lineedit *le, int c);
+#define LINEEDIT_OK 0
+#define LINEEDIT_FAILED -1
+#define LINEEDIT_ENTER -2
+#define LINEEDIT_TAB -3
+
+int32_t lineedit_backspace(struct lineedit *le);
+#define LINEEDIT_BACKSPACE_OK 0
+#define LINEEDIT_BACKSPACE_FAILED -1
+
+int32_t lineedit_insert_char(struct lineedit *le, int c);
+#define LINEEDIT_INSERT_CHAR_OK 0
+#define LINEEDIT_INSERT_CHAR_FAILED -1
+
+int32_t lineedit_set_print_handler(struct lineedit *le, int32_t (*print_handler)(const char *line, void *ctx), void *ctx);
+#define LINEEDIT_SET_PRINT_HANDLER_OK 0
+#define LINEEDIT_SET_PRINT_HANDLER_FAILED -1
+
+int32_t lineedit_set_prompt_callback(struct lineedit *le, int32_t (*prompt_callback)(struct lineedit *le, void *ctx), void *ctx);
+#define LINEEDIT_SET_PROMPT_CALLBACK_OK 0
+#define LINEEDIT_SET_PROMPT_CALLBACK_FAILED -1
+
+int32_t lineedit_refresh(struct lineedit *le);
+#define LINEEDIT_REFRESH_OK 0
+#define LINEEDIT_REFRESH_FAILED -1
+
+int32_t lineedit_get_cursor(struct lineedit *le, uint32_t *cursor);
+#define LINEEDIT_GET_CURSOR_OK 0
+#define LINEEDIT_GET_CURSOR_FAILED -1
+
+int32_t lineedit_set_cursor(struct lineedit *le, uint32_t cursor);
+#define LINEEDIT_SET_CURSOR_OK 0
+#define LINEEDIT_SET_CURSOR_FAILED -1
+
+int32_t lineedit_get_line(struct lineedit *le, char **text);
+#define LINEEDIT_GET_LINE_OK 0
+#define LINEEDIT_GET_LINE_FAILED -1
+
+int32_t lineedit_set_line(struct lineedit *le, const char *text);
+#define LINEEDIT_SET_LINE_OK 0
+#define LINEEDIT_SET_LINE_FAILED -1
+
+int32_t lineedit_clear(struct lineedit *le);
+#define LINEEDIT_CLEAR_OK 0
+#define LINEEDIT_CLEAR_FAILED -1
+
+int32_t lineedit_insert(struct lineedit *le, const char *text);
+#define LINEEDIT_INSERT_OK 0
+#define LINEEDIT_INSERT_FAILED -1
+
+
+
+
+#endif
+
